5fe475f5838df5719df41479436799d1e172a4ad
kirigo karanja <sharonkirigo@gmail.com>
2018-07-17 14:10:20 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/ico" href="favicon.ico" />
+    <title>Stephane | Home</title>
+
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+    <script type="text/javascript" src="engine1/jquery.js"></script>
+    <!-- End WOWSlider.com HEAD section -->
+
+    <link rel="stylesheet" href="CSS/navbar.css" type="text/css">
+    <link rel="stylesheet" href="CSS/aboutme.css" type="text/css">
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+</head>
+<body>
+<nav class="homenavbar">
+    <a href="index.php" style="float: left; padding-top: 0; margin-left: 140px;text-decoration: none"><img src="logo.png"></a>
+    <a href="" style="text-decoration:none">News</a>
+    <a href="" style="text-decoration:none">Contact</a>
+    <a href="" style="text-decoration:none">About</a>
+    <a class="active" href="index.php" style="text-decoration:none">Home</a>
+</nav>
+
+<!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
+<div id="wowslider-container1">
+    <div class="ws_images"><ul>
+            <li><a href="http://wowslider.com/vi"><img src="data1/images/slide3.jpg" alt="cssslider" title="" id="wows1_0"/></a></li>
+            <li><img src="data1/images/slide4.jpg" alt="" title="" id="wows1_1"/></li>
+        </ul></div>
+    <div class="ws_bullets"><div>
+            <a href="#" title=""><span><img src="data1/tooltips/slide3.jpg" alt=""/>1</span></a>
+            <a href="#" title=""><span><img src="data1/tooltips/slide4.jpg" alt=""/>2</span></a>
+        </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">wow slider</a> by WOWSlider.com v8.7</div>
+    <div class="ws_shadow"></div>
+</div>
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+
+<div class="about_me" style="margin-top: 5%">
+    <div class="col-xs-12 to_animate text-left" data-animation="fadeInUp">
+        <div class="side-item content-padding big-padding with_background with_shadow backgrounded-media bordered-media">
+            <div class="row vertical-center-md">
+                <div
+                        class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="item-media">
+                        <img src="IMAGES/marcus-lewis-612001-unsplash.jpg">
+                    </div>
+                </div> <!-- eof image .col-* -->
+                <div class="col-lg-7 col-md-7 col-md-push-1 col-sm-12 col-xs-12">
+                    <div class="item-content">
+                        <h2 class="section_header big highlight4">
+                            About Me </h2>
+                        <hr class="dotted_divider" /><p class="grey regular fontsize_24">
+                            <em>I have been a passionate makeup artist based in London for over 10 years</em>
+                        </p><hr class="dotted_divider" />
+                        <p>Fully qualified as both a hair and makeup technician, I now have an extensive portfolio of projects comprising of everything from bridal hair and fashion
+                            shoots to catwalk models and media stars and have recently been recognised as the latest creative talent by independent fashion and lifestyle magazine.</p>
+                        <p class="text">Marie Parsons</p>
+                        <p class="small-text grey">makeup artist</p>
+                    </div>
+                </div><!-- eof text .col-* -->
+            </div><!-- eof .row -->
+        </div><!-- eof .side-item -->
+    </div>
+</div>
+</body>
+</html>
---
+++ try.php
@@ -0,0 +1,114 @@
+<style>
+    body {
+        font-family: "Muli", sans-serif;
+        font-size: 18px;
+        font-weight: 300;
+        line-height: 27px;
+    }
+    .col-xs-12 {
+        width: 80%;
+        float: left;
+        position: relative;
+        min-height: 1px;
+        padding-right: 15px;
+        padding-left: 15px;
+    }
+
+    .text-left {
+        text-align: left;
+    }
+
+    .ls.ms .with_background {
+        background-color: #fff;
+    }
+    .with_shadow {
+        box-shadow: 2px 0 30px rgba(0, 0, 0, 0.1);
+    }
+
+    .side-item .row {
+        margin: 0;
+    }
+    .vertical-center-md {
+        -webkit-align-items: stretch;
+        align-items: stretch;
+        -webkit-box-align: stretch;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: stretch;
+    }
+    .side-item.backgrounded-media .col-md-4 {
+        width: 35%;
+    }
+    .side-item .row [class*="col-"] {
+        margin: 0;
+        padding: 0;
+    }
+    .col-lg-1, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9 {
+        float: left;
+    }
+    .media_cover {
+        background-size: cover;
+    }
+    .bg_overlay, .media_cover {
+        bottom: 0;
+        left: 0;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .media_cover > img {
+        visibility: hidden;
+    }
+    img, figure {
+        height: auto;
+        max-width: 100%;
+    }
+    img {
+        vertical-align: middle;
+    }
+    img {
+        border: 0;
+    }
+    .side-item.backgrounded-media .item-media {
+        overflow: visible;
+    }
+    .backgrounded-media .item-media {
+        padding: 45px;
+    }
+    .backgrounded-media .item-media {
+        padding: 30px;
+    }
+    .backgrounded-media .item-media {
+        padding: 20px;
+    }
+</style>
+
+<div class="col-xs-12 to_animate text-left" data-animation="fadeInUp">
+    <div class="side-item content-padding big-padding with_background with_shadow backgrounded-media bordered-media">
+        <div class="row vertical-center-md">
+            <div
+                class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="item-media">
+                    <img src="IMAGES/marcus-lewis-612001-unsplash.jpg">
+                </div>
+            </div> <!-- eof image .col-* -->
+            <div
+                class="col-lg-7 col-md-7 col-md-push-1 col-sm-12 col-xs-12">
+                <div class="item-content">
+                    <h2 class="section_header big highlight4">
+                        About Me </h2>
+                    <hr class="dotted_divider" /><p class="grey regular fontsize_24">
+                        <em>I have been a passionate makeup artist based in London for over 10 years</em>
+                    </p><hr class="dotted_divider" />
+                    <p>With a degree in art and an ability to connect with each individual client, I feel privileged to work within such a broad and creative industry.</p>
+                    <p>Fully qualified as both a hair and makeup technician, I now have an extensive portfolio of projects comprising of everything from bridal hair and fashion
+                        shoots to catwalk models and media stars and have recently been recognised as the latest creative talent by independent fashion and lifestyle magazine.</p>
+                    <h4 class="big regular highlight bottommargin_0 topmargin_20">Marie Parsons</h4>
+                    <p class="small-text grey">makeup artist</p>								</div>
+            </div><!-- eof text .col-* -->
+        </div><!-- eof .row -->
+    </div><!-- eof .side-item -->
+</div>
